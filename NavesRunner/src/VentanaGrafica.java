f6a424f6ce320cd1a0079df01ef0140c6684b6f5
IbaiCarracedo <ibaicarracedo@opendeusto.es>
2019-10-15 13:56:53 +0200

    - Inicio de la VentanaGrafica - Creadas las clases panel del juego, login y seleccion de modo

---
+++ NavesRunner/src/VentanaGrafica.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+
+public class VentanaGrafica extends JFrame {
+	
+	public VentanaGrafica() {
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setTitle("Alien Runner");
+		setSize(800,600);
+		setLocationRelativeTo(null);
+		
+//		JPanel pJuego = new JPanel(); // Iniciamos los diferentes paneles
+//		JPanel pLogin = new JPanel(); // Sin mostrarlos hasta que sea necesario
+//		JPanel pSeleccion = new JPanel(); // Comenzaremos con la parte del juego > guardar partida > login 
+		
+		
+		add(pJuego);
+		
+		setVisible(true);
+	}
+	
+	public static void main (String[] args) {
+		VentanaGrafica v = new VentanaGrafica();
+	}
+}
